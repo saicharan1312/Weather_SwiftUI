aff75cdb58cda82689b3784c64343ffb0b5d30d8
Sai Charan <saicharan12nov@gmail.com>
2024-07-01 20:09:28 -0400

    Initial Commit

---
+++ Weather_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather_SwiftUI
+//
+//  Created by Sai Charan Thummalapudi on 7/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather_SwiftUI/Weather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftUIApp.swift
+//  Weather_SwiftUI
+//
+//  Created by Sai Charan Thummalapudi on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
